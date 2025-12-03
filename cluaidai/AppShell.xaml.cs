4353d0c0241cada5c308d0f9e5aafe95af99c6b1
Ethan Hagen <ethan@rptindustries.com>
2025-12-03 12:42:41 -0600

    Add project files.

---
+++ cluaidai/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace cluaidai
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ cluaidai/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "cluaidai")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "cluaidai.Pages")]
